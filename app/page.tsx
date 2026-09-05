5c631b1ae3cb433d2bf2d358fc8a7ca735fc6fe1
Edanur <pktzleda@gmail.com>
2026-09-06 01:47:00 +0300

    Initial Next.js setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white">
+      <h1 className="text-4xl font-bold text-black">
+        Bussion Platform
+      </h1>
+    </main>
+  );
+}
